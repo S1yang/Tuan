fa7f14c7148ac21c6294d6620d693c36a6df85ae
S1yang <1320535451@qq.com>
2021-09-16 13:52:23 +0800

    init

---
+++ src/main/java/cn/edu/njust/tuan/TuanApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.njust.tuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuanApplication.class, args);
+    }
+
+}
