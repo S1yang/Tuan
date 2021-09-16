4077eaf40c0f80ef65eed2ad7f6779f9ae3fb72a
S1yang <1320535451@qq.com>
2021-09-16 16:06:19 +0800

    update

--- src/main/java/cn/edu/njust/tuan/TuanApplication.java
+++ src/main/java/cn/edu/njust/tuan/TuanApplication.java
@@ -2,8 +2,9 @@ package cn.edu.njust.tuan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class TuanApplication {
 
     public static void main(String[] args) {
